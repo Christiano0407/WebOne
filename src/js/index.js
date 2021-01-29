615c582d208eb9904c5ac5ebeb1422b619ba4193
Christiano0407 <placebo0704@gmail.com>
2021-01-29 12:13:32 -0600

    header-remove

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,18 @@ const btnHamburger = document.querySelector(`#btnHamburger`);
 const body = document.querySelector(`body`);
 const header = document.querySelector(`.header`); 
 const overlay = document.querySelector(`.overlay`);
+const fadeElems = document.querySelector(`.has-fade`);
+
 
 btnHamburger.addEventListener(`click`, function() {
     console.log("Click Hamburger");
 
-    if(btnHamburger.classList.contains(`open`)){
-       btnHamburger.classList.remove(`open`);
+    if(header.classList.contains(`open`)){
+       body.classList.remove(`noscroll`);
+       header.classList.remove(`open`);
+
     }else {
-        btnHamburger.classList.add(`open`);
+        body.classList.add(`noscroll`);
+        header.classList.add(`open`);
     }
 })
