20e8cfe02728969b727671f04ef514aae5231526
Christiano0407 <placebo0704@gmail.com>
2021-01-29 11:29:11 -0600

    header-overlay

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 const btnHamburger = document.querySelector(`#btnHamburger`);
+const body = document.querySelector(`body`);
+const header = document.querySelector(`.header`); 
+const overlay = document.querySelector(`.overlay`);
 
 btnHamburger.addEventListener(`click`, function() {
     console.log("Click Hamburger");
