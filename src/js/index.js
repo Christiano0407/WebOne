1908b7568072ae29338dc9daec42bfeb1da903cf
Christiano0407 <placebo0704@gmail.com>
2021-01-29 10:56:06 -0600

    hamburger_js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const btnHamburger = document.querySelector(`#btnHamburger`);
+
+btnHamburger.addEventListener(`click`, function() {
+    console.log("Click Hamburger");
+
+    if(btnHamburger.classList.contains(`open`)){
+       btnHamburger.classList.remove(`open`);
+    }else {
+        btnHamburger.classList.add(`open`);
+    }
+})
